6d339273d1c5f523a0be4fa8adc87cea46d22e49
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-13 08:24:36 -0300

    feat: create messages schema

--- src/database/schemas/message.schema.ts
+++ src/database/schemas/message.schema.ts
@@ -1,12 +1,8 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
-import { v4 as uuidv4 } from 'uuid';
 
 @Schema()
 export class Message extends Document {
-    @Prop({ type: String, default: uuidv4 })
-    id: String;
-
     @Prop({ required: true })
     text: String;
 
