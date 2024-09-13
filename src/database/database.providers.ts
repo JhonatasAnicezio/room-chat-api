25d9cdf1374023a0205ae61d958b62be42443529
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-13 09:33:56 -0300

    feat: use dote.env

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(),
+      mongoose.connect(`${process.env.MONGO_URI}`),
   },
 ];
