8f7e581702d2ba1bd3743cfc004c7d1517f64dc9
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-13 09:33:37 -0300

    feat: hability .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    MessagesModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Faz com que as variáveis estejam disponíveis globalmente
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
