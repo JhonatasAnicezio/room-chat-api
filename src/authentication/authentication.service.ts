cb516d4e835ca687f8b36f87fa462165065a5b56
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-16 11:20:28 -0300

    feat: create service singIn and add senEmailVerification at Register

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterDto } from './dto/register-dto';
 import { auth } from 'src/firebase/firebase-config';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword, UserCredential } from 'firebase/auth';
 
 @Injectable()
 export class AuthenticationService {
     async register({ email, password }: RegisterDto) {
-        return await createUserWithEmailAndPassword(auth, email, password);
+        return await createUserWithEmailAndPassword(auth, email, password)
+            .then(() => sendEmailVerification(auth.currentUser))
+    }
+
+    async signIn({ email, password }: { email: string, password: string }) {
+        return await signInWithEmailAndPassword(auth, email, password)
+            .then(({ user }: UserCredential) => console.log(user.emailVerified));
     }
 }
