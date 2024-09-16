bd2986dea46ea846ddc0ba2ce88900f275bf7cd9
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-16 11:19:40 -0300

    feat: create route sing-in

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { RegisterDto } from './dto/register-dto';
+import { signInDto } from './dto/sign-in-dto';
 
 @Controller('authentication')
 export class AuthenticationController {
-  constructor(private readonly authenticationService: AuthenticationService) {}
+  constructor(private readonly authenticationService: AuthenticationService) { }
 
   @Post()
-  async register({ email, password }: RegisterDto) {
+  async register(@Body() { email, password }: RegisterDto) {
     await this.authenticationService.register({ email, password });
+  }
+
+  @Post('sing-in')
+  async singIn(@Body() { email, password }: signInDto) {
+    await this.authenticationService.signIn({ email, password });
   }
 }
